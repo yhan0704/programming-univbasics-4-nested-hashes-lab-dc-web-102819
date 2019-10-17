b90528c9fb7230c67dc8de79acf858c865a3d496
pleasant-controller-2669 <han20@umbc.edu>
2019-10-17 19:33:28 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -105,7 +105,7 @@ def changing_alan
      }
 
     alans_new_info = "GUI"
-    changing_alan[:alan_kay][:known_for] = alans_new_info
+    programmer_hash[:alan_kay][:known_for] = alans_new_info
      
 end
 
